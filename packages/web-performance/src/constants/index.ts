751f51845f2f9930f348a3cf37dea6e8c7181a01
liuzibin <1835064412@qq.com>
2022-11-09 23:34:37 +0800

    fps

--- packages/web-performance/src/constants/index.ts
+++ packages/web-performance/src/constants/index.ts
@@ -9,4 +9,6 @@ export enum PerformanceNameType {
     FCP = 'first-contentful-paint',  // 首次内容绘制 页面从开始加载到页面内容的任何部分在屏幕上完成渲染的时间
     LCP = 'largest-contentful-paint',  // 最大内容绘制
     FID = "first-input",  //  次输入延迟 (FID)
+    FPS = "fps",  //  fps帧率
+    TTFB = "TTFB"
 }
--- packages/web-performance/src/metrics/getFPS.ts
+++ packages/web-performance/src/metrics/getFPS.ts
@@ -1,8 +1,53 @@
-import { resolve } from "path"
+import { retainToFixed } from "../utils/calculate.js";
 
 
+/**
+ * 使用 requestAnimationFrame 计算 FPS
+ *   
+ */
 const fetchFPS = (): Promise<number> => {
   return new Promise((resolve, reject) => {
+
+    let lastFrameTime = Date.now();
+    let frameCount = 0;
+    const fpsQueue = [];
+    let timerId = null;
+
+
+    const calculateFPS = () => {
+
+      const now = Date.now();
+      frameCount++;
+
+      // 执行大于1s
+      if (now > 1000 + lastFrameTime) {
+        const fps = Math.round(frameCount * 1000 / (now - lastFrameTime))
+        fpsQueue.push(fps);
+        frameCount = 0;
+        lastFrameTime = + new Date();
+
+        // 循环10次
+        if (fpsQueue.length > 10) {
+          cancelAnimationFrame(timerId);
+
+          const totalFPS = fpsQueue.reduce((sum: number, fps: number) => {
+            sum = sum + fps;
+            return sum;
+          }, 0)
+          const resultFPS: number = retainToFixed(totalFPS / fpsQueue.length, 2);
+          resolve && resolve(resultFPS)
+
+        } else {
+          timerId = requestAnimationFrame(calculateFPS);
+        }
+
+      } else {
+        timerId = requestAnimationFrame(calculateFPS);
+      }
+
+    }
+
+    calculateFPS();
 
   })
 }
@@ -14,6 +59,6 @@ const fetchFPS = (): Promise<number> => {
  */
 export const getFPS = (): void => {
   fetchFPS().then((fps: number) => {
-
+    console.log('获取fps--------', fps);
   });
 }
